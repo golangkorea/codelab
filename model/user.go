bfe3092140966b1299c0de7a4fed48d0ce606599
mingrammer <mingrammer@gmail.com>
2019-03-24 16:08:02 +0900

    Refactor with gofmt

--- model/user.go
+++ model/user.go
@@ -1,7 +1,7 @@
 package model
 
 type User struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
 	Picture string `json:"picture"`
 }
